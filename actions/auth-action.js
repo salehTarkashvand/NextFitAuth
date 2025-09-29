46c9326c1c6a1f72a00b5381c8de1d92b101a447
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-09-29 16:51:19 +0330

    Checking for Email Duplication

--- actions/auth-action.js
+++ actions/auth-action.js
@@ -2,6 +2,7 @@
 
 import { hashUserPassword } from "@/lib/hash";
 import { createUser } from "@/lib/user";
+import { redirect } from "next/navigation";
 
 export default async function signup(prevState, formData) {
   const email = formData.get("email");
@@ -16,8 +17,22 @@ export default async function signup(prevState, formData) {
     errors.password = "password must be at least 8 characters long";
   }
   if (Object.keys(errors).length > 0) {
-    return {errors};
+    return { errors };
   }
-  const hashedPassword = hashUserPassword(password)
-  createUser(email , hashedPassword)
+
+  const hashedPassword = hashUserPassword(password);
+
+  try {
+    createUser(email, hashedPassword);
+  } catch (error) {
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return {
+        errors: {
+          email: "It seems like an account for the chosen email already exists",
+        },
+      };
+    }
+    throw error
+  }
+  redirect('/training')
 }
